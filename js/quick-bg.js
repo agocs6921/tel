63b38f7c2be1e97042f3dc93ec10430ff27bc1f1
agocs6921 <47692733+agocs6921@users.noreply.github.com>
2021-04-29 19:35:13 +0200

    Kommentek a js-ben

--- js/quick-bg.js
+++ js/quick-bg.js
@@ -1,8 +1,15 @@
+/* Ez a script ez egy gyors háttér beállítást csinál
+a 'bg' egyedi attribútum segítségével */
+
+// lekérjük az összes 'bg' attribútumú elemet és átmegyünk mindegyikek
 document.querySelectorAll("[bg]").forEach((element) => {
+    // Megnézzük hogy ürese és ha igen akkor vissza megyünk
     if (element.getAttribute("bg") == null) return;
 
+    /* Megpróbáljuk beállítani a hátteret a 'style' attribútummal
+    mert valamiért element.style az nem működött :P */
     try{
-        element.setAttribute("style", `background-image: url(${element.getAttribute("bg")}); ${element.getAttribute("style")}`);
+        element.setAttribute("style", `background-image: url(${element.getAttribute("bg")}); ${element.getAttribute("style") ? element.getAttribute("style") : ""}`);
     }catch(err){
         console.log(err);
     }
