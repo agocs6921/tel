edf389f6926a5e5c1869d1084c15e56993ec4121
agocs6921 <47692733+agocs6921@users.noreply.github.com>
2021-05-02 11:07:52 +0200

    kis változás

--- js/pool-calc.js
+++ js/pool-calc.js
@@ -8,9 +8,8 @@ function calculator() {
     var l = values[0], w = values[1], d = values[2];
     if (!(l && w && d))
         return;
-    var r = document.querySelector("#result");
     var _a = pool_calc(l, w, d), human_count = _a[0], volume = _a[1];
-    r.innerHTML = "<b title=\"" + human_count + "\">" + Math.round(human_count) + "</b> ember f\u00E9r el egy <b title=\"" + volume + "\">" + Math.round(volume) + "</b> m<sup>3</sup>-es medenc\u00E9ben";
+    document.querySelector("#result").innerHTML = "<b title=\"" + human_count + "\">" + Math.round(human_count) + "</b> ember f\u00E9r el egy <b title=\"" + volume + "\">" + Math.round(volume) + "</b> m<sup>3</sup>-es medenc\u00E9ben";
 }
 function pool_calc(length, width, depth) {
     var volume = length * width * depth;
