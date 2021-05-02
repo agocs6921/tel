18def9a2d9a63d7769cfba50f79a585d7294c246
agocs6921 <47692733+agocs6921@users.noreply.github.com>
2021-05-02 14:09:12 +0200

    Új függvény!

--- js/current-year.js
+++ js/current-year.js
@@ -1,3 +1,6 @@
+function get_current_year() {
+    return new Date().getFullYear();
+}
 document.querySelectorAll(".current-year").forEach(function (element) {
     element["innerText"] = new Date().getFullYear();
 });
