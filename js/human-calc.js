c67be37bc7695596c355414558dace29a727d834
agocs6921 <47692733+agocs6921@users.noreply.github.com>
2021-05-03 21:42:40 +0200

    Felesleges dokumentáció

--- js/human-calc.js
+++ js/human-calc.js
@@ -2,15 +2,47 @@
 // Agócs Kevin írta
 //
 /* Ez a fájl haszontalan, ne használd xD  */
+/**
+ * Ez a függvény haszontalan, ne használd!
+ * @param {Element} value_source Ez az az elem amely hívta a függvény, 'input'-nak kell lennie
+ * @param {string|Element} output_target Az 'id'-e vagy maga az elem ami majd ki fogja jelezni az eredményt
+ * @returns {void} Ez neked nem ad semmit :(
+ */
 function human_calculator(value_source, output_target) {
-    var value = value_source["value"];
+    /**
+     * Az érték amit használunk arra hogy kiszámoljuk a víz mennyiségét ami ennyibe belefér
+     * @type {number}
+     */
+    const value = value_source["value"];
+    /**
+     * Szövegdoboz ami kifogja írni a vég eredényt
+     * @type {Element}
+     */
+    const text_box = typeof output_target == "string" ? document.querySelector(`${(output_target.startsWith("#") ? "" : "#")}${output_target}`) : output_target;
     if (!value)
         return;
+    /**
+     * Amennyi víz elfér *value* mennyiségű emberben
+     * @type {number}
+     */
     const liters = human_calc(value);
-    document.querySelector(`#${output_target}`).innerHTML = `<b title="${liters}">${Math.round(liters)}</b> liter víz férne el <b title="${value}">${Math.round(value)}</b> felnőtt emberben`;
+    text_box.innerHTML = `<b title="${liters}">${Math.round(liters)}</b> liter víz férne el <b title="${value}">${Math.round(value)}</b> felnőtt emberben`;
 }
+/**
+ * Ez a függvény számolja a litereket
+ * @param {number} human_count Emberek száma
+ * @returns {number} Literben víz amennyi elfér *human_count* mennyiségű emberben
+ */
 function human_calc(human_count) {
+    /**
+     * Emberi térfogat
+     * @type {number}
+     */
     const human_volume = 0.062;
+    /**
+     * 1 liter víz térfogata
+     * @type {number}
+     */
     const l = 0.001;
     return ((human_volume * 0.70) * human_count) / l;
 }
--- js/quick-bg.js
+++ js/quick-bg.js
@@ -3,11 +3,17 @@
 //
 /* Ez a script ez egy gyors háttér beállítást ér el
 a 'bg' egyedi attribútum segítségével */
-// Lekérjük az összes 'bg' attribútumú elemet és átmegyünk mindegyikek
-document.querySelectorAll("[bg]").forEach(element => {
-    // Megnézzük hogy ürese és ha igen akkor vissza megyünk
-    if (element.getAttribute("bg") == null)
-        return;
-    // Beállítjuk a hátteret
-    element["style"].backgroundImage = `url(${element.getAttribute("bg")})`;
-});
+/**
+ * Lekérjük az összes 'bg' attribútumú elemet és átmegyünk mindegyikek és az attribútumba írt url alapján beállítjuk a hátteret
+ * @returns {void} Semmit nem kapsz tőle :(
+ */
+function initQuickBgs() {
+    document.querySelectorAll("[bg]").forEach(element => {
+        // Megnézzük hogy ürese és ha igen akkor vissza megyünk
+        if (element.getAttribute("bg") == null)
+            return;
+        // Beállítjuk a hátteret
+        element["style"].backgroundImage = `url(${element.getAttribute("bg")})`;
+    });
+}
+initQuickBgs();
